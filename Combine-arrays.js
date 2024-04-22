aca4b7e0a56fe7a69c3d121a6340011f6a6b1c73
Azhar Abbass <azharabbass786.com@gmail.com>
2024-04-23 01:02:43 +0500

    updated combine arrays

--- Combine-arrays.js
+++ Combine-arrays.js
@@ -1,18 +1,31 @@
 
-        // Combine Arrays
-        function combineArrays(arr1, arr2) {
-            // To remove duplicates we can use 'Set class'
-            const combinedArr = [...new Set([...arr1, ...arr2])];
-            return combinedArr;
-        }
-        console.log(combineArrays([1, 2, 3, 4], [4, 5, 6]));
+    // Combine Arrays with spread operater
+    function combineArrays_spread(arr1, arr2) {
+        // To remove duplicates we can use 'Set class'
+        const combinedArr = [...new Set([...arr1, ...arr2])];
+        return combinedArr;
+    }
+    console.log(combineArrays_spread([1, 2, 3, 4], [4, 5, 6]));
 
-        // Combine arrays with a diffrent method.
-        function combineArrays2(arr1, arr2) {
-            for (let i = 0; i < arr2.length; i++) {
-                arr1.push(arr2[i])
-            };
-            return arr1;
-        }
-        console.log(combineArrays2([1, 2, 3], [4, 5, 6]));
+    // Combine arrays with push method.
+    let array1 = [1,2,3, 4, 5, 6, 7];
+    let array2 = [8, 9, 10];
+    function combineArrays_push(arr1, arr2) {
+        for (let i = 0; i < arr2.length; i++) {
+            arr1.push(arr2[i])
+        };
+        return arr1;
+    }
+    console.log(combineArrays_push(array1, array2));
+
+    // Combine arrays with reduce method
+    function combineArrays_reduce(arr1, arr2){
+        let newArr = arr2.reduce((arr, item) =>{
+            arr.push(item)
+            return arr;
+        }, arr1);
+
+        return newArr;
+    }
+    console.log(combineArrays_reduce([1, 2, 3], [4, 5, 6]));
     
